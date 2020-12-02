9ae94f330ec4fee55a6778e4174451a57244dffe
BinTruong <bintruong97@gmail.com>
2020-12-02 20:05:19 +0700

    Library Toggle

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngleLeft,
@@ -41,7 +40,7 @@ const Player = ({audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, so
         <p>{getTime(songInfo.currentTime)}</p>
         <input
           min={0}
-          max={songInfo.duration}
+          max={songInfo.duration || 0}
           value={songInfo.currentTime}
           onChange={dragHandler}
           type="range"
